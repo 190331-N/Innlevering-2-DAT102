7a8e7bf4178ec8f4425311b5b8e3ff3e8a760ff0
ToyotaSTAE86 <bonorway@gmail.com>
2026-02-12 10:58:36 +0100

    a

--- src/ParantesSjekker.java
+++ src/ParantesSjekker.java
@@ -1,2 +1,18 @@
 public class ParantesSjekker {
+
+
+
+    private boolean erStartParentes(char c) {
+        return c == '{' || c == '[' || c == '(';
+    }
+
+    private boolean erSluttParentes(char c) {
+        return c == '}' || c == ']' || c == ')';
+    }
+
+    private boolean erParentesPar(char start, char slutt) {
+        return (start == '{' && slutt == '}')
+                || (start == '[' && slutt == ']')
+                || (start == '(' && slutt == ')');
+    }
 }
