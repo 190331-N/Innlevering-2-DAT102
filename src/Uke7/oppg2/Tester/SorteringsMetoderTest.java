1088ccdde1876e977e2a39c4f8e593ebd40d56da
HansArne04 <hansarnemid@gmail.com>
2026-02-17 15:32:35 +0100

    2

--- src/Uke7/oppg2/Tester/SorteringsMetoderTest.java
+++ src/Uke7/oppg2/Tester/SorteringsMetoderTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class InnsettingMetodeTest {
+public class SorteringsMetoderTest {
 
     private Integer[] a;
 
