130edc1db3b8848c69282b3ea8e4d78c77647d15
HansArne04 <hansarnemid@gmail.com>
2026-02-17 13:01:53 +0100

    a

---
+++ src/Uke7/oppg2/SorteringsMetoder/utvalgsSortering.java
@@ -0,0 +1,30 @@
+package Uke7.oppg2.SorteringsMetoder;
+
+public class utvalgsSortering {
+    public static <T extends Comparable<? super T>> void utvalgssortering(T[] a, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            // Finn indeksen til minste element i resten av tabellen
+            for (int j = i + 1; j < n; j++) {
+                if (a[j].compareTo(a[minIndex]) < 0) {
+                    minIndex = j;
+                }
+            }
+
+            // Bytt plass viss vi fann ein mindre
+            if (minIndex != i) {
+                swap(a, i, minIndex);
+            }
+        }
+    }
+    public static <T extends Comparable<? super T>> void utvalgssortering(T[] a) {
+        utvalgssortering(a, a.length);
+    }
+
+    public static void swap(Object[] a, int i, int j) {
+        Object temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
