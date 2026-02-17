3818fc5463043c02d6d7cfba4380c3e5e10d8506
HansArne04 <hansarnemid@gmail.com>
2026-02-17 12:32:35 +0100

    a

--- src/Uke6/adt/StabelADT.java
+++ src/Uke6/adt/StabelADT.java
@@ -1,9 +1,39 @@
 package Uke6.adt;
 
+import java.util.EmptyStackException;
+
 public interface StabelADT<T> {
-    void push(T value);
-    T pop();
-    T peek();
-    boolean isEmpty();
-    void clear();
+
+    /**
+     * Adds a new entry to the top of this stack.
+     *
+     * @param newEntry An object to be added to the stack.
+     */
+    public void push(T newEntry);
+
+    /**
+     * Removes and returns this stack's top entry.
+     *
+     * @return The object at the top of the stack.
+     * @throws EmptyStackException if the stack is empty before the operation.
+     */
+    public T pop();
+
+    /**
+     * Retrieves this stack's top entry.
+     *
+     * @return The object at the top of the stack.
+     * @throws EmptyStackException if the stack is empty.
+     */
+    public T peek();
+
+    /**
+     * Detects whether this stack is empty.
+     *
+     * @return True if the stack is empty.
+     */
+    public boolean isEmpty();
+
+    /** Removes all entries from this stack. */
+    public void clear();
 }
